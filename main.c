66704fa715be4c84db6cd415131e6487c3238f13
Kel <97975851+KelAnims@users.noreply.github.com>
2024-09-20 21:27:50 +0200

    Fix: improve output and use curses.h

--- main.c
+++ main.c
@@ -1,22 +1,35 @@
-#include <ncurses.h>
+#include <curses.h>
+#include <stdio.h>
+#include <string.h>
 #include <time.h>
 #include <unistd.h>
+
+void cntr_printw(WINDOW *win, char str[]) {
+  int cntr_row, cntr_col;
+  getmaxyx(win, cntr_row, cntr_col);
+  cntr_row /= 2;
+  cntr_col = cntr_col/2 - strlen(str)/2;
+  mvprintw(cntr_row, cntr_col, "%s\n", str);
+}
 
 int main(void) {
   time_t rawtime;
   struct tm ftime;
-  int row, col;
+  WINDOW *win;
+  char buf[10] = {0};
 
-  initscr();
+  if( ( win = initscr() ) == NULL) {
+    fprintf(stderr, "Error: initscr(): could not initialize screen");
+    return 1;
+  }
   cbreak();
   noecho();
-
-  getmaxyx(stdscr, row, col);
   
   while(1) {
     time(&rawtime);
     localtime_r(&rawtime, &ftime);
-    mvprintw(row/2, col/2 - 4, "%d:%d:%d", ftime.tm_hour, ftime.tm_min, ftime.tm_sec);
+    snprintf(buf, 10, "%02d:%02d:%02d", ftime.tm_hour, ftime.tm_min, ftime.tm_sec);
+    cntr_printw(win, buf);
     refresh();
     sleep(1);
   }
